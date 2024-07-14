5f1a7b93c4066cad0de4dc5f4c40ade8f8dbd5d2
swarooprana7 <swarooprana77@gmail.com>
2024-07-14 16:07:20 +0545

    doctor images added

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { FaFacebookF, FaInstagram, FaYoutube } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
+import "../styles/footer.css";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className="footer">
+          <div className="footer-links">
+            <h3>Links</h3>
+            <ul>
+              <li>
+                <NavLink to={"/"}>Home</NavLink>
+              </li>
+              <li>
+                <NavLink to={"/doctors"}>Doctors</NavLink>
+              </li>
+              <li>
+                <NavLink to={"/appointments"}>Appointments</NavLink>
+              </li>
+              <li>
+                <NavLink to={"/notifications"}>Notifications</NavLink>
+              </li>
+              <li>
+                <HashLink to={"/#contact"}>Contact Us</HashLink>
+              </li>
+              <li>
+                <NavLink to={"/profile"}>Profile</NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className="social">
+            <h3>Social links</h3>
+            <ul>
+              <li className="facebook">
+                <a
+                  href="https://www.facebook.com/"
+                  target={"_blank"}
+                  rel="noreferrer"
+                >
+                  <FaFacebookF />
+                </a>
+              </li>
+              <li className="youtube">
+                <a
+                  href="https://www.youtube.com/"
+                  target={"_blank"}
+                  rel="noreferrer"
+                >
+                  <FaYoutube />
+                </a>
+              </li>
+              <li className="instagram">
+                <a
+                  href="https://www.instagram.com/"
+                  target={"_blank"}
+                  rel="noreferrer"
+                >
+                  <FaInstagram />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="footer-bottom">
+          Made by{" "}
+          <a
+            href="https://www.linkedin.com/in/dunna-swaroop"
+            target="_blank"
+            rel="noreferrer"
+          >
+            swaroop
+          </a>{" "}
+          © {new Date().getFullYear()}
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
